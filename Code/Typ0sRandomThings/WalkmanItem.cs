63a885a3a37e20316119e97f4d62699af9a76528
Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
2025-02-24 22:37:14 -0700

    Squashed commit of the following:
    commit 083781ba55678006bc6f98796c03ae497cc0f7f2
    Author: Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
    Date:   Mon Feb 24 22:36:38 2025 -0700
    
        Adding code
    
    commit 4acc1a675cca50661a35a3292d1c4a4a158900ec
    Author: Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
    Date:   Mon Feb 24 22:02:13 2025 -0700
    
        Adding basic mod stuff
    
    commit 753fd3cfb83c4277f53f59d07249b512e61c1cc6
    Author: Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
    Date:   Mon Feb 24 21:55:02 2025 -0700
    
        Setting things up

---
+++ Code/Typ0sRandomThings/WalkmanItem.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Typ0sRandomThings
+{
+
+    public class WalkmanItem : GrabbableObject
+    {
+        //litterally just the boombox but it says walkman instead. thats it.
+        //changed it to keep playing while in pocket but lower pitch and volume
+
+        public AudioSource walkmanAudio;
+
+        public AudioClip[] musicAudios;
+
+        public AudioClip[] stopAudios;
+
+        public System.Random musicRandomizer;
+
+        private StartOfRound playersManager;
+
+        private RoundManager roundManager;
+
+        public bool isPlayingMusic;
+
+        private float noiseInterval;
+
+        private int timesPlayedWithoutTurningOff;
+
+        public override void Start()
+        {
+            base.Start();
+            playersManager = FindObjectOfType<StartOfRound>();
+            roundManager = FindObjectOfType<RoundManager>();
+            musicRandomizer = new System.Random(playersManager.randomMapSeed - 10);
+        }
+
+       
+
+        public override void ItemActivate(bool used, bool buttonDown = true)
+        {
+            base.ItemActivate(used, buttonDown);
+            StartMusic(used);
+        }
+
+        private void StartMusic(bool startMusic, bool pitchDown = false)
+        {
+            if (startMusic)
+            {
+                walkmanAudio.clip = musicAudios[musicRandomizer.Next(0, musicAudios.Length)];
+                walkmanAudio.pitch = 1f;
+                walkmanAudio.Play();
+            }
+            else if (isPlayingMusic)
+            {
+                if (pitchDown)
+                {
+                    StartCoroutine(musicPitchDown());
+                }
+                else
+                {
+                    walkmanAudio.Stop();
+                    walkmanAudio.PlayOneShot(stopAudios[UnityEngine.Random.Range(0, stopAudios.Length)]);
+                }
+                timesPlayedWithoutTurningOff = 0;
+            }
+            isBeingUsed = startMusic;
+            isPlayingMusic = startMusic;
+        }
+
+        private IEnumerator musicPitchDown()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                yield return null;
+                walkmanAudio.pitch -= 0.033f;
+                if (walkmanAudio.pitch <= 0f)
+                {
+                    break;
+                }
+            }
+            walkmanAudio.Stop();
+            walkmanAudio.PlayOneShot(stopAudios[UnityEngine.Random.Range(0, stopAudios.Length)]);
+        }
+
+        public override void UseUpBatteries()
+        {
+            base.UseUpBatteries();
+            StartMusic(startMusic: false, pitchDown: true);
+        }
+
+        public override void PocketItem()
+        {
+            base.PocketItem();
+            //walkmanAudio.pitch -= 0.05f;
+            walkmanAudio.volume = 0.7f;
+            //StartMusic(startMusic: false);
+        }
+
+        public override void EquipItem()
+        {
+            //walkmanAudio.pitch += 0.05f;
+            walkmanAudio.volume = 1f;
+            base.EquipItem();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (isPlayingMusic)
+            {
+                if (noiseInterval <= 0f)
+                {
+                    noiseInterval = 1f;
+                    timesPlayedWithoutTurningOff++;
+                    roundManager.PlayAudibleNoise(transform.position, 16f, 0.9f, timesPlayedWithoutTurningOff, noiseIsInsideClosedShip: false, 5);
+                }
+                else
+                {
+                    noiseInterval -= Time.deltaTime;
+                }
+                if (insertedBattery.charge < 0.05f)
+                {
+                    walkmanAudio.pitch = 1f - (0.05f - insertedBattery.charge) * 4f;
+                }
+            }
+        }
+    }
+}
+
---
+++ Code/Typ0sRandomThings/plugin.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+using System;
+
+using BepInEx;
+using HarmonyLib;
+using LethalLib.Modules;
+using UnityEngine;
+
+namespace Typ0sRandomThings
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+
+    public class Plugin : BaseUnityPlugin
+    {
+
+        const string GUID = "SLDTyp0.Typ0sRandomThings";
+        const string NAME = "Typ0's Random Things";
+        const string VERSION = "2.0.1";
+
+        public static Plugin instance;
+        void Awake()
+        {
+            instance = this;
+
+            string assetDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "typ0sassets");
+            AssetBundle bundle = AssetBundle.LoadFromFile(assetDir);
+
+            Item willhat = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/Typ0's Random Things/Items/Wills Hat/Wills Hat.asset");
+            Item walkman = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/Typ0's Random Things/Items/Walkman/walkman.asset");
+            Item waterbottle = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/Typ0's Random Things/Items/water bottle/Waterbottle.asset");
+            Item fireextinguisher = bundle.LoadAsset<Item>("Assets/LethalCompany/Mods/Typ0's Random Things/Items/FireExtinguisher/firextinguisher.asset");
+
+
+            Logger.LogInfo("Loading Typ0's Random Things");
+
+            NetworkPrefabs.RegisterNetworkPrefab(willhat.spawnPrefab);
+            Utilities.FixMixerGroups(willhat.spawnPrefab);
+            Items.RegisterScrap(willhat, 40, Levels.LevelTypes.All);
+            Logger.LogInfo("Loaded Will's Entire Personality Into The Game");
+
+            NetworkPrefabs.RegisterNetworkPrefab(waterbottle.spawnPrefab);
+            Utilities.FixMixerGroups(waterbottle.spawnPrefab);
+            Items.RegisterScrap(waterbottle, 75, Levels.LevelTypes.All);
+            Logger.LogInfo("Loaded Waterbottle");
+
+            NetworkPrefabs.RegisterNetworkPrefab(fireextinguisher.spawnPrefab);
+            Utilities.FixMixerGroups(fireextinguisher.spawnPrefab);
+            Items.RegisterScrap(fireextinguisher, 75, Levels.LevelTypes.All);
+            Logger.LogInfo("Loaded Fire extinguisher");
+
+            NetworkPrefabs.RegisterNetworkPrefab(walkman.spawnPrefab);
+            Utilities.FixMixerGroups(walkman.spawnPrefab);
+            Items.RegisterScrap(walkman, 65, Levels.LevelTypes.All);
+            Logger.LogInfo("Loaded walkman");
+
+            Logger.LogInfo("Loaded All Items!");
+
+            Logger.LogInfo("Finished Loading Typ0's Random Things");
+        }
+    }
+}
+
